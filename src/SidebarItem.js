a1704b3abfcc6795c12e7baba3b32ffaf5a06b3d
roshni27s <roshni.rx@gmail.com>
2022-09-27 23:45:07 +0530

    first commit

---
+++ src/SidebarItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SidebarItem({name, active, handleClick}) {
+  return (
+    <button className={`sidebar-item ${active?'active':''}`}
+    onClick={handleClick}>{name}</button>
+  )
+}
+
+export default SidebarItem
